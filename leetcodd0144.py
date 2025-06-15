4752761a9b335d706140437661c8c81c10cf374d
Shirley0122-li <xinru0122@gmail.com>
2025-06-14 22:30:21 -0400

    new leetcode

---
+++ leetcodd0144.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [root]
+        res = []
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        
+        # approach 2: recursive
+        def preorder(node):
+            if not node:
+                return []
+            
+            res.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
